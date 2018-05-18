c4f02c97984ef5a848f33c700deec8cc2952e66f
CarlosAburto <carlos.ab.pe@gmail.com>
2018-05-17 19:25:33 -0500

    Primera prueba wsrest

--- ProyectoOmegaCliente/build/web/home.jsp
+++ ProyectoOmegaCliente/build/web/home.jsp
@@ -10,15 +10,43 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>JSP Page</title>
-    </head>
-    <body>
+
+        <script>
+            
+            function callRESTfulWebService(id, method, target) {
+                var ajaxRequest;
+                if (window.XMLHttpRequest) {
+                    ajaxRequest = new XMLHttpRequest(); // IE7+, Firefox, Chrome, Opera, Safari
+                } else {
+                    ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP"); // IE6, IE5
+                }
+                ajaxRequest.onreadystatechange = function () {
+                    if (ajaxRequest.readyState == 4 &&
+                            (ajaxRequest.status == 200 || ajaxRequest.status == 204)) {
+                        document.getElementById(id).innerHTML = ajaxRequest.responseText;
+                    }
+                }
+                ajaxRequest.open(method, target, true /*async*/);
+                ajaxRequest.setRequestHeader("Content-Type", "text/html");
+                ajaxRequest.send();
+            }
+ 
         
-        <%
+        </script>
+
+    </head>
+     <%
             HttpSession mySession = request.getSession();
             String username = (String) mySession.getAttribute("username");
-            String userid   = (String) mySession.getAttribute("userid");
+            String userid = (String) mySession.getAttribute("userid");
+            String funcion = "callRESTfulWebService('tablas','GET','http://localhost:8080/ProyectoOmegaRest/webresources/tablas/" +userid + "')";
         %>
-        
-        <h1>Bienvenido <%= username %></h1>
+    <body>
+
+        <h1>Bienvenido <%= username%></h1>
+        <button onclick="<%= funcion %>">Ver Tablas creadas</button>
+        <h2>Las tablas que has creado son: </h2>
+        <div id="tablas"></div>
+
     </body>
 </html>
---
+++ ProyectoOmegaRest/src/java/webservices/tablas.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webservices;
+
+import java.util.ArrayList;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
+
+/**
+ * REST Web Service
+ *
+ * @author carlosaburto
+ */
+@Path("tablas/{userid}")
+public class tablas {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of tablas
+     */
+    public tablas() {
+    }
+
+    /**
+     * Retrieves representation of an instance of webservices.tablas
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("text/html")
+    public String getXml(@PathParam("userid")String userid) {
+        //@QueryParam("userid")String userid
+//        ArrayList nombres = (ArrayList) obtenerTablasUsuario(userid);
+//        String[] resultado = new String[nombres.size()];
+//        
+//        for (int i = 0; i < nombres.size(); i++) 
+//            resultado[i] = (String) nombres.get(i);
+//        
+//        return resultado;
+        return "hola "+userid;
+    }
+
+    /**
+     * PUT method for updating or creating an instance of tablas
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/xml")
+    public void putXml(String content) {
+    }
+
+    private static java.util.List<java.lang.Object> obtenerTablasUsuario(java.lang.String idUsuario) {
+        webservices.Registros_Service service = new webservices.Registros_Service();
+        webservices.Registros port = service.getRegistrosPort();
+        return port.obtenerTablasUsuario(idUsuario);
+    }
+    
+    
+    
+}
